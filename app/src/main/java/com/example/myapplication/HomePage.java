cceaf64feadb023235f3f947b5fdc15484a18065
tomas <tomtom.inbar@gmail.com>
2022-11-27 17:27:51 +0200

    home page to menu works

--- app/src/main/java/com/example/myapplication/HomePage.java
+++ app/src/main/java/com/example/myapplication/HomePage.java
@@ -3,21 +3,39 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements View.OnClickListener {
 
     ImageView login,menu,settings,foodMenu;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+//        menu = (ImageView) findViewById(R.id.menuImg);
+    }
 
 
-        menu = (ImageView) findViewById(R.id.menuImg);
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View view)
+    {
+        switch(view.getId()){
+            case R.id.menuFoodImg:
+                startActivity(new Intent(this, MenuPage.class));
+                break;
 
+        }
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
