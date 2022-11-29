0a261de89772d2b6411bf54152f6f8b899793bbd
yosim <JosefTheKing17>
2022-11-29 21:09:05 +0200

    Authentication Work

--- app/src/main/java/com/example/myapplication/HomePage.java
+++ app/src/main/java/com/example/myapplication/HomePage.java
@@ -3,30 +3,32 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class HomePage extends AppCompatActivity implements View.OnClickListener {
+public class HomePage extends AppCompatActivity implements View.OnClickListener   {
 
     ImageView login,menu,settings,foodMenu;
+
+
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
 //        menu = (ImageView) findViewById(R.id.menuImg);
     }
 
 
     @SuppressLint("NonConstantResourceId")
     @Override
-    public void onClick(View view)
-    {
-        switch(view.getId()){
+    public void onClick(View view) {
+        switch (view.getId()) {
             case R.id.menuFoodImg:
                 startActivity(new Intent(this, MenuPage.class));
                 break;
@@ -34,14 +36,9 @@ public class HomePage extends AppCompatActivity implements View.OnClickListener 
                 startActivity(new Intent(this, Login.class));
                 break;
             case R.id.homeImg:
-                startActivity(new Intent(this,HomePage.class));
+                startActivity(new Intent(this, HomePage.class));
                 break;
 
         }
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-        super.onPointerCaptureChanged(hasCapture);
     }
 }
--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -1,14 +1,20 @@
 package com.example.myapplication;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,8 +28,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Arrays;
@@ -32,26 +41,28 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
     FirebaseFirestore firestore;
-    private TextView register,forgotPassword;
-    private EditText editTextEmail,editTextPassword;
-    private ImageView fbBtn,twiterBtn,googleBtn;
+    private TextView register, forgotPassword;
+    private EditText editTextUserName, editTextPassword;
+    private ImageView fbBtn, twiterBtn, googleBtn;
     private Button login;
+    private ProgressBar progressBar;
 
     CallbackManager callbackManager;
     GoogleSignInOptions googleSignInOptions;
     GoogleSignInClient googleSignInClient;
 
 
-    
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
         callbackManager = CallbackManager.Factory.create();
         googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions);
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        FirebaseUser currentUser = mAuth.getCurrentUser();
 
         LoginManager.getInstance().registerCallback(callbackManager,
                 new FacebookCallback<LoginResult>() {
@@ -72,7 +83,9 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 });
 
         firestore = FirebaseFirestore.getInstance();
-        mAuth = FirebaseAuth.getInstance();
+
+        editTextUserName = (EditText) findViewById(R.id.username);
+        editTextPassword = (EditText) findViewById(R.id.pass_login);
 
         register = (TextView) findViewById(R.id.register);
         register.setOnClickListener(this);
@@ -80,7 +93,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         forgotPassword = (TextView) findViewById(R.id.forgotPass);
         forgotPassword.setOnClickListener(this);
 
-        fbBtn =(ImageView) findViewById(R.id.facebook);
+        fbBtn = (ImageView) findViewById(R.id.facebook);
         fbBtn.setOnClickListener(this);
 
         googleBtn = (ImageView) findViewById(R.id.google);
@@ -89,15 +102,17 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         login = (Button) findViewById(R.id.login);
         login.setOnClickListener(this);
 
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
 
     }
 
     @SuppressLint("NonConstantResourceId")
     @Override
     public void onClick(View view) {
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.register:
-                startActivity(new Intent(this,Register.class));
+                startActivity(new Intent(this, Register.class));
                 break;
             case R.id.forgotPass:
                 startActivity(new Intent(this, ForgotPassword.class));
@@ -109,27 +124,64 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 signIn();
                 break;
             case R.id.login:
-                startActivity(new Intent(this, HomePage.class));
+                signInWithMail();
                 break;
-                
+
         }
 
     }
 
     private void signIn() {
         Intent signInIntent = googleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent,1000);
+        startActivityForResult(signInIntent, 1000);
     }
+
+    public void signInWithMail() {
+
+        String mail = editTextUserName.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (mail.isEmpty()) {
+            editTextUserName.setError("Email is required");
+            editTextUserName.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(mail).matches()) {
+            editTextUserName.setError("Please provide valid email");
+            editTextUserName.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            editTextPassword.setError("Min password length should be 6 characters!");
+            editTextPassword.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(mail, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        startActivity(new Intent(this, HomePage.class));
+                        Toast.makeText(Login.this, "Authentication Succeed.",Toast.LENGTH_SHORT).show();
+
+
+                    } else {
+                        Toast.makeText(Login.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                    }
+
+                });
+                progressBar.setVisibility(View.VISIBLE);
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1000){
+        if (requestCode == 1000) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 task.getResult(ApiException.class);
                 navigateToSecondActivity();
-            }catch (ApiException e){
+            } catch (ApiException e) {
                 Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
             }
         }
