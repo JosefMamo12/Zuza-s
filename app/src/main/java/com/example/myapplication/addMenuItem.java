e71a21418289bbcdd04da2bd5f3214a3f20a92f9
tomas <tomtom.inbar@gmail.com>
2022-12-11 15:22:10 +0200

    separated item validation, added item price.

--- app/src/main/java/com/example/myapplication/addMenuItem.java
+++ app/src/main/java/com/example/myapplication/addMenuItem.java
@@ -19,7 +19,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class addMenuItem extends AppCompatActivity implements View.OnClickListener
 {
 
-    private EditText editName, editCategory;
+    private EditText editName, editCategory, editPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,6 +33,7 @@ public class addMenuItem extends AppCompatActivity implements View.OnClickListen
 
         editName = (EditText) findViewById(R.id.add_item_name);
         editCategory =(EditText) findViewById(R.id.add_item_category);
+        editPrice = (EditText)  findViewById(R.id.add_item_price);
 
     }
 
@@ -55,29 +56,57 @@ public class addMenuItem extends AppCompatActivity implements View.OnClickListen
     {
         String name = editName.getText().toString().trim();
         String category = editCategory.getText().toString().trim();
-        if (name.isEmpty())
-        {
-            editName.setError("Name required.");
-            editName.requestFocus();
+        String price = editPrice.getText().toString().trim();
+
+        if (invalidInput(name, category, price))
             return;
-        }
-        if (category.isEmpty())
-        {
-            editCategory.setError("category required.");
-            editCategory.requestFocus();
-            return;
-        }
 
         // Define new item object and add into database under menuItems.
-        menuItem newItem = new menuItem(name, category);
+        menuItem newItem = new menuItem(name, category, Double.parseDouble(price));
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("menuItems");
 
         myRef.setValue(newItem);
         Toast t = new Toast(this);
-        t.setText("OK!");
+        t.setText("הוספת פריט הושלמה.");
         t.show();
         finish();
+    }
+
+    /**
+     * Validates input of item to be added.
+     * @param name product name
+     * @param category product category
+     * @param price product price
+     * @return false if only all the given inputs are valid and can be used to create a new item.
+     */
+    private boolean invalidInput(String name, String category, String price)
+    {
+        if (name.isEmpty())
+        {
+            editName.setError("Name required.");
+            editName.requestFocus();
+            return true;
+        }
+        if (category.isEmpty())
+        {
+            editCategory.setError("Category required.");
+            editCategory.requestFocus();
+            return true;
+        }
+        if (price.isEmpty())
+        {
+            editPrice.setError("Price required.");
+            editPrice.requestFocus();
+            return true;
+        }
+        else if (Double.parseDouble(price) < 0)
+        {
+            editPrice.setError("Please input a non-negative price.");
+            editPrice.requestFocus();
+            return true;
+        }
+        return false;
     }
 
 }
