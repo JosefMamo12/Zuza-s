b883eb9be78fab76596245f6c601711a1dd616d2
JosefTheKing17 <yosimami50@gmail.com>
2023-01-01 21:20:22 +0200

    Cart View updated

--- app/src/main/java/com/example/myapplication/ShoppingCartView.java
+++ app/src/main/java/com/example/myapplication/ShoppingCartView.java
@@ -9,6 +9,6 @@ public class ShoppingCartView extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_cart_view);
+        setContentView(R.layout.activity_shopping_cart);
     }
 }
